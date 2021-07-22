081024bfab02a1f0a238d4e7cebe5afeca8a40f3
RavinakS <ravinasingh697@gmail.com>
2021-07-23 03:49:04 +0530

    not able to undersatand what is exactly NaN

--- tryCatch.js
+++ tryCatch.js
@@ -4,4 +4,25 @@ const readline = require('readline-sync')
 
 const user = readline.question("type a string: ");
 
-console.log(user);
+checker = 0;
+for(index=0; index<user.length; index++){
+    try{
+        num = parseInt(user[index], 10);
+        if(num === 'NaN'){
+            console.log("In");
+            checker++;
+        }else{
+            console.log('Out');
+        }
+    }
+    catch{
+        continue;
+    }
+};
+
+console.log(checker);
+if(checker > 0){
+    console.log('Approved');
+}else{
+    console.log('Not Approved');
+}
