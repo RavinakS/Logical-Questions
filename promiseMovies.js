b03851152a243cfd2555bd72f1ebf8740b7e464e
RavinakS <ravinasingh697@gmail.com>
2021-07-27 12:29:58 +0530

    for each loop is not working with the resolved data which is in array

--- promiseMovies.js
+++ promiseMovies.js
@@ -1,11 +1,17 @@
 const axios = require('axios');
 
-const movieUrl = 'https://ghibliapi.herokuapp.com/films';
+const movieUrl = 'https://ghibliapi.herokuapp.com/films'; // copied from a site "Community"
 
-const getApi = axios.get(movieUrl);
-console.log(getApi);
-getApi.then((resolvedData)=>{
-    console.log(resolvedData);
-}, (rejectedData)=>{
+const getApi = axios.get(movieUrl); // axoi.get() method returns a promise
+console.log(getApi); // a promise - { pending }
+getApi.then(
+    (resolvedData)=>{
+        console.log(typeof resolvedData);
+    // console.log('Successfully retrieved our list of movies');
+    // resolvedData.forEach(aMovie => {
+    //     console.log(`${aMovie['title']}, ${aMovie['release_date']}`);
+    // })
+}, 
+    (rejectedData)=>{
     console.log(rejectedData);
 })
