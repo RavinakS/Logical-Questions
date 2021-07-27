edf36e4ca2dbb1fd1d73dabb9859f65b00e5baad
RavinakS <ravinasingh697@gmail.com>
2021-07-27 11:59:42 +0530

    axios.get() method returns a promise

--- promiseMovies.js
+++ promiseMovies.js
@@ -4,3 +4,8 @@ const movieUrl = 'https://ghibliapi.herokuapp.com/films';
 
 const getApi = axios.get(movieUrl);
 console.log(getApi);
+getApi.then((resolvedData)=>{
+    console.log(resolvedData);
+}, (rejectedData)=>{
+    console.log(rejectedData);
+})
