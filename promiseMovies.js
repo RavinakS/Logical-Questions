d916673dd9d3cefb5b3133d5306e7da0fa299275
RavinakS <ravinasingh697@gmail.com>
2021-07-27 15:04:24 +0530

    retrived the list of movies

--- promiseMovies.js
+++ promiseMovies.js
@@ -6,11 +6,12 @@ const getApi = axios.get(movieUrl); // axoi.get() method returns a promise
 console.log(getApi); // a promise - { pending }
 getApi.then(
     (resolvedData)=>{
-        console.log(typeof resolvedData);
-    // console.log('Successfully retrieved our list of movies');
-    // resolvedData.forEach(aMovie => {
-    //     console.log(`${aMovie['title']}, ${aMovie['release_date']}`);
-    // })
+        // console.log(resolvedData.data); // "data" is the key of the the whole main object, "data", it's a array has all movie details.
+        console.log('Successfully retrieved our list of movies');
+        count = 1;
+        resolvedData.data.forEach(aMovie => {
+            console.log(`${count++}. ${aMovie['title']} (${aMovie['release_date']})`);
+    })
 }, 
     (rejectedData)=>{
     console.log(rejectedData);
