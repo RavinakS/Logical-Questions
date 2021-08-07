a604cb37e69b8e4a12d63c38b5ce2d96253437ee
RavinakS <ravinasingh697@gmail.com>
2021-08-08 04:06:56 +0530

    **#**

--- numberLineJumps.js
+++ numberLineJumps.js
@@ -1,16 +1,18 @@
 function kangaroo(x1, v1, x2, v2) {
     if(v1>v2){
         var count = 0;
-        for(count; count<x2; count++){
+        var end_time = x2;
+        for(count; count<=end_time; count++){
             x1 = x1 + v1;
             x2 = x2 + v2;
             if(x1===x2){
                 return "YES";
             }
         }
+        return "NO";
     }else{
         return "NO";
     }
 }
 
-console.log(kangaroo(0, 2, 5, 3));
+console.log(kangaroo(21, 6, 47, 3));
