8df3e35af7809e2fee372ecb6da9a72263e968a3
RavinakS <ravinasingh697@gmail.com>
2021-08-07 15:40:34 +0530

    ^^^

--- time_conversion.js
+++ time_conversion.js
@@ -1,15 +1,23 @@
 function timeConversion(s) {
     let hours = [0, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
     let hour = parseInt(s[0]+s[1]);
-    console.log(hour);
+    console.log(typeof hour);
     let time = s[s.length-2];
     if(time==="P"){
-        if(hour===0){
-            return (`00:${s[3]}${s[4]}:${s[6]}${s[7]}`)
+        console.log("1");
+        return (`${hours[hour]}:${s[3]}${s[4]}:${s[6]}${s[7]}`);
+    }else{
+        if(hour===12){
+            console.log("2");
+            return (`00:${s[3]}${s[4]}:${s[6]}${s[7]}`);
+        }else if(9<hour && hour<12){
+            console.log("3");
+            return (`${hour}:${s[3]}${s[4]}:${s[6]}${s[7]}`)
         }else{
-            return (`${hours[hour]}:${s[3]}${s[4]}:${s[6]}${s[7]}`);
+            console.log("4");
+            return (`0${hour}:${s[3]}${s[4]}:${s[6]}${s[7]}`)
         }
     }
 }
-s = "10:05:45PM"
+s = "06:40:03AM"
 console.log(timeConversion(s));
