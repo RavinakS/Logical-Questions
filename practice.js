622d19ef5c6a429d9e134fa51b19194a791643ab
RavinakS <ravinasingh697@gmail.com>
2021-08-07 12:09:44 +0530

    experimenting

---
+++ practice.js
@@ -0,0 +1,15 @@
+// num = 1/6;
+// console.log(num);
+// console.log(num.toFixed(6));
+
+let pos_values = 3;
+let nag_values = 2;
+let zeros = 1;
+let n = 6;
+let prop_postv_values = (pos_values/n);
+let prop_nagtv_values = (nag_values/n);
+let prop_zero_values = (zeros/n);
+
+console.log(prop_postv_values.toFixed(6));
+console.log(prop_nagtv_values.toFixed(6));
+console.log(prop_zero_values.toFixed(6));
