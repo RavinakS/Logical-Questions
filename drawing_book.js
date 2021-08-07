7b02d66461335c1630a992f3ac23f3b3251ee14d
RavinakS <ravinasingh697@gmail.com>
2021-08-08 02:24:37 +0530

    **

--- drawing_book.js
+++ drawing_book.js
@@ -1,25 +1,35 @@
 function pageCount(n, p) {
     var starting = 0;
-    var count = 0;
-    for(count; count<n; count+=2){
+    var count = 1;
+    while(count<n){
         starting+=1;
-        if(count>=p){
-            break
+        if(count>p){
+            starting-=1;
+            break;
+        }else if(count === p){
+            starting-=1;
+            break;
         }
+        count+=2
     }
     var ending = 0;
-    for(n; n>p; n-=2){
+    while(n>p){
+        n-=2
         ending+=1;
-        if(n<=p){
+        if(n<p){
+            ending-=1;
+            break;
+        }else if(n==p){
             break;
         }
     }
+
     if(starting>ending){
         return ending;
     }
     return starting;
 }
 
-const n = 5;
+const n = 6;
 const p = 4;
 console.log(pageCount(n, p));
