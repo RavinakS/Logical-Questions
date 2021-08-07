46f27b60601cf895dcafcecaa9b2f66be0393d33
RavinakS <ravinasingh697@gmail.com>
2021-08-08 01:05:35 +0530

    @@@

--- apple_n_orange.js
+++ apple_n_orange.js
@@ -3,17 +3,15 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
     let total_orranges = 0;
     let fruit_a;
     let fruit_b;
-    let all_apples = apples.length;
-    let all_oranges = oranges.length;
     let count;
-    if(all_apples>all_oranges){
+    if(apples.length>oranges.length){
         count = 0;
-        for(count; count<all_apples; count++){
+        for(count; count<apples.length; count++){
             fruit_a = a + apples[count];
             if(fruit_a>=s && fruit_a<=t){
                 total_apples = total_apples + 1;
             }
-            if(count<all_oranges){
+            if(count<oranges.length){
                 fruit_b = b + oranges[count];
                 if(fruit_b>=s && fruit_b<=t){
                     total_orranges = total_orranges + 1;
@@ -22,14 +20,14 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
         }
         console.log(total_apples);
         console.log(total_orranges);
-    }else{
+    }else if(oranges.length>=apples.length){
         count = 0;
-        for(count; count<all_apples; count++){
+        for(count; count<apples.length; count++){
             fruit_b = b + oranges[count];
             if(fruit_b>=s && fruit_b<=t){
                 total_orranges = total_orranges + 1;
             }
-            if(count<all_apples){
+            if(count<apples.length){
                 fruit_a = a + apples[count];
                 if(fruit_a>=s && fruit_a<=t){
                     total_apples = total_apples + 1;
@@ -41,3 +39,11 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
     }
 
 }
+
+const s = 7;
+const t = 11;
+const a = 5
+const b = 15;
+const apples = [-2, 2, 1];
+const oranges = [5, -6];
+countApplesAndOranges(s, t, a, b, apples, oranges);
