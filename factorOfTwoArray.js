9e1a9f6020e54abfe612738a0ad565f45870ad58
RavinakS <ravinasingh697@gmail.com>
2021-08-11 10:58:51 +0530

    removed one loop

--- factorOfTwoArray.js
+++ factorOfTwoArray.js
@@ -1,30 +1,42 @@
+// number of factors between two array
+// 1. The elements of the first array are all factors of the integer being considered
+// 2. The integer being considered is a factor of all elements of the second array
+
 function getTotalX(a, b) {
     const last_elemnt_a = a[a.length - 1];
     let i = last_elemnt_a;
     let count = 0;
     for(i; i<=b[0]; i+=last_elemnt_a){
         let j = 0;
-        for(j; j<a.length; j++){
-            if(i % a[j] != 0){
-                console.log("hi");
-                break;
-            }
-        }
-        if(j === a.length){
-            j = 0;
-            for(j; j<b.length; j++){
+        for(j; j<b.length; j++){
+            if(j<a.length){
+                if(i % a[j] != 0 || b[j] % i != 0){
+                    break;
+                }
+            }else{
                 if(b[j] % i != 0){
-                    console.log(i, "hiii");
                     break;
                 }
             }
-            if(j===b.length){
-                console.log(i);
-                count+=1;
-            }
         }
+        if(j===b.length){
+            console.log(i);
+            count+=1;
+        }
+        // if(j === a.length){
+        //     j = 0;
+        //     for(j; j<b.length; j++){
+        //         if(b[j] % i != 0){
+        //             break;
+        //         }
+        //     }
+        //     if(j===b.length){
+        //         console.log(i);
+        //         count+=1;
+        //     }
+        // }
     }
-    return count
+    return count;
 }
 
 const a = [2, 4];
