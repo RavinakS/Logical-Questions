7ba329c93fe44e00d4f14ed79323b80f12b71b6b
RavinakS <ravinasingh697@gmail.com>
2021-08-11 10:44:22 +0530

    number of factors between two sets

---
+++ factorOfTwoArray.js
@@ -0,0 +1,32 @@
+function getTotalX(a, b) {
+    const last_elemnt_a = a[a.length - 1];
+    let i = last_elemnt_a;
+    let count = 0;
+    for(i; i<=b[0]; i+=last_elemnt_a){
+        let j = 0;
+        for(j; j<a.length; j++){
+            if(i % a[j] != 0){
+                console.log("hi");
+                break;
+            }
+        }
+        if(j === a.length){
+            j = 0;
+            for(j; j<b.length; j++){
+                if(b[j] % i != 0){
+                    console.log(i);
+                    break;
+                }
+            }
+            if(j===b.length){
+                console.log(i);
+                count+=1;
+            }
+        }
+    }
+    return count
+}
+
+const a = [2, 4];
+const b = [18, 32, 96];
+console.log(getTotalX(a, b));
