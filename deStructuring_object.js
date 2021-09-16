516af58092f1474119377a13d33b3f670ac6c8d4
RavinakS <ravinasingh697@gmail.com>
2021-09-16 16:01:41 +0530

    %%

---
+++ deStructuring_object.js
@@ -0,0 +1,13 @@
+const obj = {
+    name: "Ravina",
+    age: 21,
+    from: "Navgurukul",
+    aducation: "12th",
+    sex: "Female"
+}
+
+const {from, sex} = obj;
+
+console.log(from);
+// console.log(aducation);
+console.log(sex);
